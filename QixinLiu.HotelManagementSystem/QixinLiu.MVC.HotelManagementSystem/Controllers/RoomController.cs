d2c17d86a1e2b5c0827fc885d1467b1a29e2ff55
sunliuqixin <sunliuqixin@gmail.com>
2022-01-24 13:29:34 -0500

    Added the get booking details feature

---
+++ QixinLiu.HotelManagementSystem/QixinLiu.MVC.HotelManagementSystem/Controllers/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QixinLiu.MVC.HotelManagementSystem.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
